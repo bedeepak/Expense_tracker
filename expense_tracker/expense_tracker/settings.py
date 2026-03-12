3b0212bfba93404da8c29d98f968f3abffb10818
Bruno <239401904+bedeepak@users.noreply.github.com>
2026-03-12 12:43:56 +0530

    included login logout function

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -115,3 +115,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDECT_URL = '/expenses/add/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
