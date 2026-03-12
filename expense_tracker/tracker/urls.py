c0cc9ebae7160570b7207f2332cdb027d9e2edda
Bruno <239401904+bedeepak@users.noreply.github.com>
2026-03-12 20:40:43 +0530

    added delete and edit page paths

--- expense_tracker/tracker/urls.py
+++ expense_tracker/tracker/urls.py
@@ -5,4 +5,6 @@ urlpatterns =[
     path('', views.dashboard, name='dashboard'),
     path('add/', views.add_expense, name='add_expense'),
     path('signup/', views.signup, name='signup'),
+    path('edit/<int:id>/',views.edit_expense, name ='edit_expense'),
+    path('delete/<int:id>/',views.delete_expense,name='delete_expense'),
 ]
