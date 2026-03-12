8143da3d9608d5cce2047b58439ec95f5e3995c3
Bruno <239401904+bedeepak@users.noreply.github.com>
2026-03-12 22:35:48 +0530

    added export path

--- expense_tracker/tracker/urls.py
+++ expense_tracker/tracker/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from .views import export_expenses_csv
 from . import views
 
 urlpatterns =[
     path('', views.dashboard, name='dashboard'),
     path('add/', views.add_expense, name='add_expense'),
+    path('export-csv/', export_expenses_csv, name='export_csv'),
     path('signup/', views.signup, name='signup'),
     path('edit/<int:id>/',views.edit_expense, name ='edit_expense'),
     path('delete/<int:id>/',views.delete_expense,name='delete_expense'),
